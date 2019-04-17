2376e07db1e7bb4a8e94034f3e0c8d467ac9b1e0
ycortes94 <ycortes94@yahoo.com>
2019-04-16 17:07:04 -0700

    Add files via upload
    Added beginning file

---
+++ Scheduling.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int main(int argc, char **argv)
+{
+   /*FIFO*/
+
+   /*SJF*/
+
+   /*BJF*/
+
+   /*STCF*/
+
+   /*RR*/
+
+   return 0;
+}
