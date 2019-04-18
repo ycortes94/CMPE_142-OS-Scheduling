4390ac204fd41cb8221df6be10dc077d1b6f6414
Yosimy Cortes <ycortes94@yahoo.com>
2019-04-17 20:03:15 -0700

    changed arrays to dynamic arrays and worked on FIFO

--- Scheduling.c
+++ Scheduling.c
@@ -3,47 +3,42 @@
 #include <sys/wait.h>
 
 //    /*FIFO*/
-int FIFO(int *array1, int *array2, int *array3)
+void FIFO(int *arr1, int *arr2, int *arr3)
 {
-   
-   int x = sizeof(array1);
-   printf("%d\n\n", x);
-   for (int i = 0; i < x; i++) {
-      printf("%d\n", array1[i]);
+   int x = sizeof(arr1)/2;
+   int y = sizeof(arr2)/2;
+   int z = sizeof(arr3)/2;
+
+   int bt[100], waitTime[100], turnaroundTime[100], avgWaitTime = 0, avgTurnaroundTime = 0, i, j;
+   waitTime[0] = 0; //waiting time for first process is 0
+
+   //calculating waiting time
+   for (i = 1; i < y; i++)
+   {
+      waitTime[i] = 0;
+      for (j = 0; j <= i; j++){
+         waitTime[i] += arr2[j];
+      }
+      printf("%d\n", waitTime[i]);
    }
-   int y = sizeof(array2);
-   int z = sizeof(array3);
-   //int n, bt[100], waitTime[100], turnaroundTime[100], avgWaitTime = 0, avgTurnaroundTime = 0, i, j;
 
-   // waitTime[0] = 0; //waiting time for first process is 0
+   printf("\nProcess\t\tWaiting Time\tTurnaround Time");
+   //calculating turnaround time
+   for (i = 0; i < y; i++)
+   {
+      turnaroundTime[i] = bt[i] + waitTime[i];
+      avgWaitTime += waitTime[i];
+      avgTurnaroundTime += turnaroundTime[i];
+      printf("\nP[%d]\t\t\t%d\t\t%d", arr1[i], waitTime[i], turnaroundTime[i]);
+   }
 
-   // //calculating waiting time
-   // for (i = 1; i < n; i++)
-   // {
-   //    waitTime[i] = 0;
-   //    for (j = 0; j < i; j++)
-   //       waitTime[i] += bt[j];
-   // }
-
-   // printf("\nProcess\t\tBurst Time\tWaiting Time\tTurnaround Time");
-
-   // //calculating turnaround time
-   // for (i = 0; i < n; i++)
-   // {
-   //    turnaroundTime[i] = bt[i] + waitTime[i];
-   //    avgWaitTime += waitTime[i];
-   //    avgTurnaroundTime += turnaroundTime[i];
-   //    printf("\nP[%d]\t\t%d\t\t%d\t\t%d", i + 1, bt[i], waitTime[i], turnaroundTime[i]);
-   // }
-
-   // avgWaitTime /= i;
-   // avgTurnaroundTime /= i;
-   // printf("\n\nAverage Waiting Time:%d", avgWaitTime);
-   // printf("\nAverage Turnaround Time:%d", avgTurnaroundTime);
-   return 0;
+   avgWaitTime /= i;
+   avgTurnaroundTime /= i;
+   printf("\n\nAverage Waiting Time:%d", avgWaitTime);
+   printf("\nAverage Turnaround Time:%d", avgTurnaroundTime);
 }
 /*SJF*/
-int SJF(int array1[], int array2[], int array3[])
+int SJF(int *arr1, int *arr2, int *arr3, size_t max)
 {
    int bt[20], p[20], wt[20], tat[20], i, j, n, total = 0, pos, temp;
    float avg_wt, avg_tat;
@@ -106,57 +101,61 @@ int SJF(int array1[], int array2[], int array3[])
    return 0;
 }
 //    /*BJF*/
-int BJF(int array1[], int array2[], int array3[])
+int BJF(int *arr1, int *arr2, int *arr3, size_t max)
 {
    return 0;
 }
 
 //    /*STCF*/
-int STCF(int array1[], int array2[], int array3[])
+int STCF(int *arr1, int *arr2, int *arr3, size_t max)
 {
    return 0;
 }
 
 //    /*RR*/
-int RR(int array1[], int array2[], int array3[])
+int RR(int *arr1, int *arr2, int *arr3, size_t max)
 {
    return 0;
 }
 
 int main(int argc, char **argv)
 {
-   char string[100];
    static const char filename[] = "jobs.dat";
    FILE *fp = fopen(filename, "r");
    if (fp != NULL)
    {
       int integers[100];
-
       int i = 0;
       int num;
+      int *jobIDs; //JOB_ID
+      int *arrivalTime; //Arrival_Time
+      int *duration; //Duration
+      int len=100;
+
+      jobIDs = (int*)calloc(len,sizeof(int));
+      arrivalTime = (int*)calloc(len,sizeof(int));
+      duration = (int*)calloc(len,sizeof(int));
 
       //Scan file for all ints excluding whitespaces and new lines
       while (fscanf(fp, "%d", &num) != EOF)
       {
          integers[i] = num;
          i++;
+
       }
-      int arr1[100]; //JOB_ID
-      int arr2[100]; //Arrival_Time
-      int arr3[100]; //Duration
       int x,y,z = 0;
+
       /*Take the first int of each line and put it in JOB_ID array*/
       for (int a = 0; a < i; a++)
       {
-         arr1[x] = integers[a];
-         printf("\n%d\n", arr1[x]);
+         *(jobIDs + x) = integers[a];
          a=a+2;
          x++;
       }
       /*Take the second int of each line and put it in Arrival_Time array*/
       for (int a = 1; a < i; a++)
       {
-         arr2[y] = integers[a];
+         arrivalTime[y] = integers[a];
          // printf("\n%d\n", arr2[y]);
          a=a+2;
          y++;
@@ -164,7 +163,7 @@ int main(int argc, char **argv)
       /*Take the third int of each line and put it in Duration array*/
       for (int a = 2; a < i; a++)
       {
-         arr3[z] = integers[a];
+         duration[z] = integers[a];
          // printf("\n%d\n", arr3[z]);
          a=a+2;
          z++;
@@ -178,11 +177,14 @@ int main(int argc, char **argv)
       // }
 
       //Calling each scheduling algorithm
-      FIFO(&arr1, &arr2, &arr3);
+      FIFO(jobIDs, arrivalTime, duration);
       // SJF(arr1, arr2, arr3);
       // BJF(arr1, arr2, arr3);
       // STCF(arr1, arr2, arr3);
       // RR(arr1, arr2, arr3);
+      free(jobIDs);
+      free(arrivalTime);
+      free(duration);
    }
    else
    {
